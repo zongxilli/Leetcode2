046cd2a4a23f84000a2cd4e5a7068f00b76908ba
zongxilli <zongxi2014@gmail.com>
2021-09-01 23:31:11 -0400

    9.1

--- 169.majority-element.js
+++ 169.majority-element.js
@@ -13,22 +13,22 @@ var majorityElement = function(nums) {
     
     if (nums.length <= 2) return nums[0];
 
-    let map = {};
+    let map = new Map();
 
     for (let num of nums) {
 
-        if (!map[num]) {
+        if (map.has(num)) {
 
-            map[num] = 1;
-        }
+            if (map.get(num) + 1 >= nums.length / 2) return num;
 
-        else if (++map[num] >= nums.length/2) {
+            else map.set(num, map.get(num) + 1);
 
-            return num;
         }
         
+        else map.set(num, 1);
     }
-    
+
 };
+
 // @lc code=end
 
