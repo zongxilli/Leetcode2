c1ebd81a08369724cfa5a7e7f55c709f8221fcf0
zongxilli <zongxi2014@gmail.com>
2021-09-02 14:13:29 -0400

    9.2

---
+++ 141.linked-list-cycle.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=141 lang=javascript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    
+    let fast = head;
+    let slow = head;
+
+    while (fast && fast.next) {
+
+        fast = fast.next.next;
+        slow = slow.next;
+        
+        if (fast == slow) return true;
+    }
+
+    return false;
+
+};
+// @lc code=end
+
