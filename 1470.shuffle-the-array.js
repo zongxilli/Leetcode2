ba1736ebd57e1095a171b94d5dbf2b1d8bd02b9d
zongxilli <zongxi2014@gmail.com>
2021-09-01 20:49:23 -0400

    9.1

--- 1470.shuffle-the-array.js
+++ 1470.shuffle-the-array.js
@@ -11,24 +11,35 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
+
+    // let result = [];
+
+    // for (let i = 0; i < nums.length; i++) {
+
+    //     if (i % 2 == 0) {
+
+    //         result.push(nums[i / 2]);
+    //     }
+
+    //     else {
+
+    //         result.push(nums[Math.floor(i / 2) + n]);
+    //     }
+
+    // }
+
+    // return result;
+
+    let result = new Array(2*n);
     
-    let result = [];
+    for (let i = 0, idx = 0; i < n; i++, idx++) {
 
-    for (let i = 0; i < nums.length; i++) {
-
-        if (i % 2 == 0) {
-
-            result.push(nums[i / 2]);
-        }
-
-        else {
-
-            result.push(nums[Math.floor(i / 2) + n]);
-        }
-
+        result[idx] = nums[i];
+        result[++idx] = nums[i + n];
     }
 
     return result;
-};
+}
+
 // @lc code=end
 
