dcd874048deafe2e02d196be17028d35a331d37f
zongxilli <zongxi2014@gmail.com>
2021-09-03 11:29:24 -0400

    9.3

---
+++ 203.remove-linked-list-elements.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=203 lang=javascript
+ *
+ * [203] Remove Linked List Elements
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    
+    while (head?.val == val) head = head.next;
+
+    let curr = head;
+
+    while (curr?.next) {
+
+        if (curr.next.val == val) {
+
+            curr.next = curr.next.next;
+        }
+
+        else curr = curr.next;
+        
+    }
+
+    return head;
+};
+// @lc code=end
+
