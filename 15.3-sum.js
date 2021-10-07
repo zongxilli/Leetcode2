d538a256b79a3a45c096fd9e73a9742c53c2e709
zongxilli <zongxi2014@gmail.com>
2021-10-07 14:39:58 -0400

    10/7

--- 15.3-sum.js
+++ 15.3-sum.js
@@ -9,8 +9,39 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-    
-};
-// @lc code=end
+const threeSum = (nums) => {
+	if (nums.length < 3) return [];
+	result = [];
 
+	nums.sort((a, b) => a - b);
+
+	for (let i = 0; i < nums.length; i++) {
+		let left = i + 1,
+			right = nums.length - 1,
+			sum = 0;
+
+		while (left < right) {
+			sum = nums[i] + nums[left] + nums[right];
+
+			if (sum == 0) {
+				result.push([nums[i], nums[left], nums[right]]);
+
+				while (nums[left] == nums[left + 1]) left++;
+
+				while (nums[right] == nums[right - 1]) right--;
+
+				left++;
+				right--;
+			} else if (sum > 0) right--;
+			else left++;
+		}
+
+		while (nums[i] == nums[i + 1]) i++;
+	}
+
+	return result;
+};
+
+//! NEED REVIEW
+
+// @lc code=end
